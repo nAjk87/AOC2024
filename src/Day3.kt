66bfc6dfb53601bca84ddb7c4527dcfa3907e8f3
Niklas Pettersson <nikpe890@gmail.com>
2024-12-03 07:55:49 +0100

    Not as ugly as before

--- src/Day3.kt
+++ src/Day3.kt
@@ -3,47 +3,37 @@ import java.io.File
 class Day3 {
 
     init {
-        var input : String = ""
+        var input = ""
         File("Day3.txt").useLines { lines ->
             lines.forEach { line ->
                 input+= line
             }
         }
 
-        val onlyMul = Regex("""mul\(\d{1,3},\d{1,3}\)""")
-        val matches = onlyMul.findAll(input).toList()
+        val onlyMulPattern = Regex("""mul\(\d{1,3},\d{1,3}\)""")
+        val matches = onlyMulPattern.findAll(input).toList()
 
-        val onlyNumbers = Regex("""\b\d{1,3},\d{1,3}\b""")
+        val almostOnlyNumbers = Regex("""\b\d{1,3},\d{1,3}\b""")
 
         var multiplySum = 0
         matches.forEach { result ->
-            val toMul = onlyNumbers.find(result.value)!!.value.split(',')
+            val toMul = almostOnlyNumbers.find(result.value)!!.value.split(',')
             multiplySum += toMul[0].toInt() * toMul[1].toInt()
         }
-        println("Day3 - Part1 : $multiplySum")
+        println("Day3 - Part 1 : $multiplySum")
 
-        //val regex = Regex("""mul\(\d{1,3},\d{1,3}\)""")
-
-        //val matches = regex.findAll(text).map { it.value }.toList()
-
-        // part2
-
-
-        val ok = "do()"
-        val faulty = "don't()"
-
-        val splitted = input.split(ok).map {
-            it.split(faulty)[0]
+        val splitted = input.split("do()").map {
+            it.split("don't()")[0]
         }
 
         var multiplySum2 = 0
         splitted.forEach { line ->
-            val tempMatches = onlyMul.findAll(line).toList()
+            val tempMatches = onlyMulPattern.findAll(line).toList()
             tempMatches.forEach { result ->
-                val toMul = onlyNumbers.find(result.value)!!.value.split(',')
+                val toMul = almostOnlyNumbers.find(result.value)!!.value.split(',')
                 multiplySum2 += toMul[0].toInt() * toMul[1].toInt()
             }
         }
-        println("Day3 : Part 2 : $multiplySum2")
+        println("Day3 - Part 2 : $multiplySum2")
     }
 }
